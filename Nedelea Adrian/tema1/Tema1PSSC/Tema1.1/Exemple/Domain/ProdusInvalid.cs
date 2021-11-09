15ebd133a4dd9b2ca43304d6da42fd6d458e2a9e
Adrian-Nedelea <84146769+Adrian-Nedelea@users.noreply.github.com>
2021-11-09 10:31:00 +0200

    Add files via upload

---
+++ Nedelea Adrian/tema1/Tema1PSSC/Tema1.1/Exemple/Domain/ProdusInvalid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Exemple.Domain
+{
+    [Serializable]
+    internal class ProdusInvalid : Exception
+    {
+        public ProdusInvalid()
+        {
+        }
+
+        public ProdusInvalid(string? message) : base(message)
+        {
+        }
+
+        public ProdusInvalid(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ProdusInvalid(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
