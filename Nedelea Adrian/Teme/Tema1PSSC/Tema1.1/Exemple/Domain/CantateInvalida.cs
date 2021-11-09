6d9a1694a515b53012ba38637cb83049fec1cd74
Adrian-Nedelea <84146769+Adrian-Nedelea@users.noreply.github.com>
2021-11-09 10:37:29 +0200

    Add files via upload

---
+++ Nedelea Adrian/Teme/Tema1PSSC/Tema1.1/Exemple/Domain/CantateInvalida.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Exemple.Domain
+{
+    [Serializable]
+    internal class CantitateInvalida: Exception
+    {
+        public CantitateInvalida()
+        {
+        }
+
+        public CantitateInvalida(string? message) : base(message)
+        {
+        }
+
+        public CantitateInvalida(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected CantitateInvalida(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Nedelea Adrian/Teme/Tema1PSSC/Tema1.1/Exemple/Program.cs
@@ -0,0 +1,69 @@
+﻿using Exemple.Domain;
+using System;
+using System.Collections.Generic;
+using static Exemple.Domain.Cos;
+
+namespace Exemple
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            var listOfCosuri = ReadListOfCosuri().ToArray();
+            CosGoll cosGoll = new(listOfCosuri);
+            ICos result = CosValidat(cosGoll);
+            result.Match(
+                whenCosGoll: emptyResult => cosGoll,
+                whenCosInvalidat: unvalitedResult => unvalitedResult,
+                whenPlatesteCoss: paidResult => paidResult,
+                whenCosValidat: validatedResult => PlatesteeCos(validatedResult)
+            );
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static List<CosGol> ReadListOfCosuri()
+        {
+            List <CosGol> listOfGrades = new();
+            do
+            {
+                //read registration number and grade and create a list of greads
+                var cantitate = ReadValue("Cantitatea produsului: ");
+                if (string.IsNullOrEmpty(cantitate))
+                {
+                    break;
+                }
+
+                var codprodus = ReadValue("Codul produslui: ");
+                if (string.IsNullOrEmpty(codprodus))
+                {
+                    break;
+                }
+                var adresa = ReadValue("Adresa:");
+                if(string.IsNullOrEmpty(adresa))
+                {
+                    break;
+                }
+
+                listOfGrades.Add(new (cantitate ,codprodus,adresa));
+            } while (true);
+            return listOfGrades;
+        }
+
+        private static ICos CosValidat(CosGoll cosgol) =>
+            random.Next(100) > 50 ?
+            new CosInvalidat(new List<CosInvalid>(), "Random errror")
+            : new CosValidat(new List<CosValid>());
+
+        private static ICos PlatesteeCos(CosValid cosvalid) =>
+            new PlatesteCoss(new List<CosValid>(), DateTime.Now);
+
+        private static string? ReadValue(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
